0650228db6f8790fec8a4dd203c91f61b434a2f5
carlocadiz <carlocadiz71@gmail.com>
2018-11-09 04:31:37 +0000

    added email notification

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,14 +2,18 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    OrderMailer.new_order(@order).deliver_now
   end
 
   def create
+   # @order = Order.find(params[:id])
     charge = perform_stripe_charge
     order  = create_order(charge)
 
     if order.valid?
+
       empty_cart!
+
       redirect_to order, notice: 'Your Order has been placed.'
     else
       redirect_to cart_path, flash: { error: order.errors.full_messages.first }
---
+++ app/mailers/order_mailer.rb
@@ -0,0 +1,13 @@
+class OrderMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.new_order.subject
+  #
+  def new_order(order)
+
+    @order = order
+    mail to: @order.email, subject: "Order ##{@order.id}"
+  end
+end
